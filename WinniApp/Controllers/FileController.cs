5cf8299f5f08ca549aa74cf010d836ea34be82f4
Henrik Schönfelder <hschoenfelder@alegri.eu>
2022-06-28 09:49:43 +0200

    Sample Project

---
+++ WinniApp/Controllers/FileController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+
+namespace WinniApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly string _directory;
+        private readonly StringBuilder _logBuilder = new StringBuilder();
+
+        public FileController()
+        {
+            _directory = Environment.GetEnvironmentVariable("HOME") ?? ".";
+        }
+
+        [HttpGet]
+        public string Get(string fileName, string fileContent)
+        {
+            _logBuilder.AppendLine($"{nameof(fileName)}: {fileName}");
+            _logBuilder.AppendLine($"{nameof(fileContent)}: {fileContent}");
+            _logBuilder.AppendLine($"{nameof(_directory)}: {_directory}");           
+
+            Create(fileName, fileContent);
+
+            return _logBuilder.ToString();
+        }
+
+        private void Create(string fileName, string fileContent)
+        {
+            Process pythonProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName= "py",
+                    Arguments = $"app.py {_directory} {fileName} {fileContent}",
+                    RedirectStandardOutput = true
+                }
+            };
+            pythonProcess.Start();
+            pythonProcess.WaitForExit(2000);
+            string pythonOutput = pythonProcess.StandardOutput.ReadToEnd();
+            _logBuilder.AppendLine($"{nameof(pythonOutput)}: {pythonOutput}");
+
+            FileInfo file = new FileInfo(Path.Combine(_directory, fileName));
+            
+            if (file.Exists)
+            {
+                _logBuilder.AppendLine($"File created: {file.FullName}");
+            }
+            else
+            {
+                _logBuilder.AppendLine($"File not created: {file.FullName}");
+            }
+        }       
+    }
+}
---
+++ WinniApp/app.py
@@ -0,0 +1,17 @@
+﻿import sys
+import os
+
+path = sys.argv[1]
+print(path)
+
+fileName = sys.argv[2]
+print(fileName)
+
+fullPath = os.path.join(path, fileName)
+print(fullPath)
+
+content = sys.argv[3]
+print(content)
+
+with open(fullPath, 'w') as f:
+    f.write(content)
